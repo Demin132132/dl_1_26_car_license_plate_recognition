cc683431e5d84f66c2436d8bf5a56489769d2ea7
Alexey_Demin <dal@veeroute.com>
2022-10-11 19:33:15 +0300

    fix inference.py and del result.txt

--- inference.py
+++ inference.py
@@ -4,19 +4,25 @@ from os.path import isfile, join
 import torch
 import cv2
 import pytesseract
+import fastwer
+import easyocr
 
 
-input_dir = 'inputs/'
+input_dir = 'test/'
 output_dir = 'outputs/'
 model_path = 'models/best.pt'
 
+# reader = easyocr.Reader(['ru'])
 
 files = [f for f in listdir(input_dir) if isfile(join(input_dir, f))]
+files = sorted(files)
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
 model.eval()
 
-for f in files:
+ref = ["КО09ЕЕ97", "Е007ВР71", "Х215ЕР125", "В151РМ35", "В286ММ77", "Р104АВ142", "В606OO64", "С070НУ42"]
+
+for index, f in enumerate(files):
 
     img = cv2.imread(input_dir + f)
 
@@ -33,13 +39,17 @@ for f in files:
 
     img = img[ymin:ymax, xmin:xmax]
 
-    custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'  # только заглавные
+    custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789АВЕКМНОРСТУ'
 
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    text = pytesseract.image_to_string(img, lang='rus', config=custom_config)
+    text = pytesseract.image_to_string(img, lang='rus', config=custom_config).upper()
 
+    # text = reader.readtext(img_rgb)
+
+    print(f)
     print(text)
 
-    with open('result.txt', 'a') as file:
-        file.write(text)
-        file.write("\n")
+    cer = fastwer.score_sent(text, ref[index], char_level=True)
+    wer = fastwer.score_sent(text, ref[index], char_level=False)
+
+    print(cer, wer, "\n")
